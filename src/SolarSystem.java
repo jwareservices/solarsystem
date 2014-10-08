67fc9bb188e541fae71a527326a5901b6a679bbc
Paul Jackson <jwareservices@gmail.com>
2014-10-08 17:26:58 -0400

    Update SolarSystem.java

--- src/SolarSystem.java
+++ src/SolarSystem.java
@@ -1,15 +1,31 @@
+package org.jware.solarsys;
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 J. Paul Jackson <jwareservices@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import processing.core.*;
-//import processing.opengl.*;
 
 /**
+ * File: SolarSystem.java Created On: 05/01/2013
  *
- * @author Paul <jwarecbt@yahoo.com>
- */
+ * @author J. Paul Jackson <jwareservices@gmail.com>
+ *
+ * Purpose: To provide a set of general utility features for the library.
+ * */
+ 
 public class SolarSystem extends PApplet {
 
     PFont label = createFont("Tahoma", 96);
